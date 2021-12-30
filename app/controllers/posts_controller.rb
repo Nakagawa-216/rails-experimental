58899a5ee8c9753d3a96d9fc773b2f5c8df2f6d3
Nakagawa-216 <rarai0626@gmail.com>
2021-12-30 09:36:48 +0000

    [Add] create-post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,20 @@
 class PostsController < ApplicationController
   def posts
+    @posts = Post.all
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:body)
   end
 end
