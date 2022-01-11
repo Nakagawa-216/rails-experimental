37290ae10e53aef1df3db83018a99e101e42acf1
Nakagawa-216 <rarai0626@gmail.com>
2022-01-11 10:21:41 +0000

    [Add] Post-comment

---
+++ app/controllers/post_comments_controller.rb
@@ -0,0 +1,14 @@
+class PostCommentsController < ApplicationController
+  def create
+    post = Post.find(params[:post_id])
+    @post_comment = PostComment.new(post_comment_params)
+    @post_comment.post_id = post.id
+    @post_comment.save!
+    redirect_to post_path(post)
+  end
+
+  private
+  def post_comment_params
+    params.require(:post_comment).permit(:body)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_05_105437) do
+ActiveRecord::Schema.define(version: 2022_01_11_100402) do
 
   create_table "post_comments", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
